fecc33d255dd82cf5e91f6ccc489008239bdd394
Nursat Zeinolla <kznursat@gmail.com>
2022-05-13 19:49:42 +0600

    added notifications

--- app/src/main/java/com/example/messengerapp/utilities/Constants.java
+++ app/src/main/java/com/example/messengerapp/utilities/Constants.java
@@ -1,5 +1,7 @@
 package com.example.messengerapp.utilities;
 
+import java.util.HashMap;
+
 public class Constants {
     public static final String KEY_COLLECTION_USERS = "users";
     public static final String KEY_NAME = "name";
@@ -23,5 +25,24 @@ public class Constants {
     public static final String KEY_RECEIVER_IMAGE = "receiverImage";
     public static final String KEY_LAST_MESSAGE = "lastMessage";
     public static final String KEY_AVAILABILITY = "availability";
+    public static final String REMOTE_MSG_AUTHORIZATION = "Authorization";
+    public static final String REMOTE_MSG_CONTENT_TYPE = "Content-Type";
+    public static final String REMOTE_MSG_DATA = "data";
+    public static final String REMOTE_MSG_REGISTRATION_IDS ="registration_ids";
 
+    public static HashMap<String, String> remoteMsgHeaders = null;
+    public static HashMap<String, String> getRemoteMsgHeaders() {
+        if(remoteMsgHeaders == null) {
+            remoteMsgHeaders = new HashMap<>();
+            remoteMsgHeaders.put(
+                    REMOTE_MSG_AUTHORIZATION,
+                    "key=AAAAY6NbNnM:APA91bE6QRss2HjeY1EsJKxIO8ETULgyMRGGGBSD2N4-DEmeCpINyZIusHOTKj9vx-7ddJpiatV6iTU9a5PIlwCX5DTdWSpRUu4sqjAmO77Vj_UT08-AmKnPe9CW29or-4_HAy0LAJnl"
+            );
+            remoteMsgHeaders.put(
+                    REMOTE_MSG_CONTENT_TYPE,
+                    "application/json"
+            );
+        }
+        return remoteMsgHeaders;
+    }
 }
